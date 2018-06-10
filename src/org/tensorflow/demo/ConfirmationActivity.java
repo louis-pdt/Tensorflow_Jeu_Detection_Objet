5b065d8d5dc0382cf71a197a55f45095120544c3
Sebastien Nico <nicosebastien@yahoo.fr>
2018-06-10 17:14:16 +0200

    nouvelle branche jeu 2 et ajout des layout avec debut des activity correspondantes

---
+++ src/org/tensorflow/demo/ConfirmationActivity.java
@@ -0,0 +1,62 @@
+package org.tensorflow.demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Trace;
+import android.util.Size;
+import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+import java.nio.ByteBuffer;
+import org.tensorflow.demo.env.ImageUtils;
+import org.tensorflow.demo.env.Logger;
+import org.tensorflow.demo.R; // Explicit import needed for internal Google builds.
+
+public class ConfirmationActivity extends Activity {
+    Button mButton1 = null;
+    Button mButton2 = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmation);
+        addListenerOnButton();}
+    public void addListenerOnButton() {
+        final Context context = this;
+        mButton1 = findViewById(R.id.yes);
+        mButton2 = findViewById(R.id.no);
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetectorActivity.class);
+                startActivity(intent);
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, RulesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
