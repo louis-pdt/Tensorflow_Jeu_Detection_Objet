c3c32cb8b0533db6110d53a0f1e67e676c550d94
Thomas Montegu <tmontegu@gmail.com>
2018-06-16 22:48:16 +0200

    Ajout de la matrice des probabilités

--- src/org/tensorflow/demo/RecognitionScoreView.java
+++ src/org/tensorflow/demo/RecognitionScoreView.java
@@ -90,6 +90,92 @@ public class RecognitionScoreView extends View implements ResultsView {
       this.mesObjetsDetecte.add(1);
   }
 
+  public String ChoixConseil( int S, int C){
+      String choix;
+      switch(S) {
+
+          case 3:
+          case 4:
+          case 5:
+          case 6:
+          case 7:
+          case 8:
+
+              choix="T";
+
+              break;
+
+          case 9:
+
+              if(C==2 || C>6){
+                  choix="T";
+              }
+              else{
+                  choix="D";
+              }
+
+              break;
+
+          case 10:
+
+              if(C>9){
+                  choix="T";
+              }
+              else{
+                  choix="D";
+              }
+
+              break;
+
+          case 11:
+
+              choix="D";
+
+              break;
+
+          case 12:
+
+              if(C<4 || C>6){
+                  choix="T";
+              }
+              else{
+                  choix="S";
+              }
+
+              break;
+
+          case 13:
+          case 14:
+          case 15:
+          case 16:
+
+              if(C>6){
+                  choix="T";
+              }
+              else{
+                  choix="S";
+              }
+
+              break;
+
+          case 17:
+          case 18:
+          case 19:
+          case 20:
+
+              choix="S";
+
+              break;
+
+          default:
+
+              System.out.println("Erreur : La somme de la main du joueur vaut 0 ou plus de 20 ou bien la carte du croupier n'est pas valide.");
+              return "ERROR";
+
+      }
+      return choix;
+  }
+
   public List<Integer> getObjetsDetecte(){
       return this.mesObjetsDetecte;
   }
